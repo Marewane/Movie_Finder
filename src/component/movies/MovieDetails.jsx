5beccce88f12004a530f3c6cce7e37864523fce1
Marewane <laamirimarwane6@gmail.com>
2025-09-30 11:38:00 +0100

    fixing conflict of branches

--- src/component/movies/MovieDetails.jsx
+++ src/component/movies/MovieDetails.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import TopBilledCast from "../persons/TopBilledCast";
+import TopBilledCast from "../persons/TopBilledCast.jsx";
 
 export default function MovieDetails(){
     const {movie_id} = useParams();
